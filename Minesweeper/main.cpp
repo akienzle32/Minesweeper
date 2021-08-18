5d47f7023236289062f4fb346b6a4a07fdf47cbd
Alec Kienzle <alec.kienzle@gmail.com>
2021-08-18 16:46:33 -0700

    first commit

--- Minesweeper/main.cpp
+++ Minesweeper/main.cpp
@@ -6,9 +6,61 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+const int ROWS = 5;
+const int COLS = 5;
+
+void makeBoard(char array[][COLS])
+{
+    
+    for (int i = 0; i < COLS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            array[i][j] = '-';
+        }
+    }
+}
+
+void placeMines(char array[][COLS])
+{
+    array[0][3] = '*';
+    array[2][1] = '*';
+    array[3][2] = '*';
+    array[4][3] = '*';
+}
+
+void printBoard(char array[][COLS])
+{
+    for (int i = 0; i < COLS; i++)
+    {
+        
+    }
+    
+    
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+int main() {
+
+    
+    char gameBoard[ROWS][COLS];
+    char realBoard[ROWS][COLS];
+    
+    makeBoard(gameBoard);
+    printBoard(gameBoard);
+    makeBoard(realBoard);
+    placeMines(realBoard);
+    
+    return(0);
 }
