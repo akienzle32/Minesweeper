0bee974a362ef2db1fcab214c26beae44fab96cc
Alec Kienzle <alec.kienzle@gmail.com>
2021-08-18 15:18:12 -0700

    Initial Commit

---
+++ Minesweeper/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Minesweeper
+//
+//  Created by Alec Kienzle on 8/18/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
